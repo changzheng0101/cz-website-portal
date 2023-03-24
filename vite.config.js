423645ab8de2e055e9538dac5377eb0cdc2de659
Admin <11265585+tianjin-polytechnic-university_0@user.noreply.gitee.com>
2023-03-24 15:56:19 +0800

    bugfix:deploy

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,10 @@
+import vue from '@vitejs/plugin-vue';
 import { fileURLToPath, URL } from 'node:url';
 import { defineConfig } from 'vite';
-import vue from '@vitejs/plugin-vue';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/cz-website-portal/',
   plugins: [vue()],
   resolve: {
     alias: {
